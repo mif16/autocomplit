0899aeccf044bc90202dfae3b6f208485722a93c
KamenshchikK <kkm.kkm2015@yandex.ru>
2017-04-07 16:22:53 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+﻿# -*- coding: utf-8 -*-
+import bottle
+
+app = bottle.app()
+
+@app.get('/')
+def index():
+	return bottle.template("index.html")
+
+@app.get("/<filepath:re:.*\.css>")
+def css(filepath):
+    return bottle.static_file(filepath, root="")
+
+
+@app.get("/<filepath:re:.*\.(eot|otf|svg|ttf|woff|woff2?)>")
+def font(filepath):
+    return bottle.static_file(filepath, root="")
+
+
+@app.get("/<filepath:re:.*\.(jpg|png|gif|ico|svg|PNG)>")
+def img(filepath):
+    return bottle.static_file(filepath, root="")
+
+
+@app.get("/js/<filepath:re:.*\.(js|jsx)>")
+def js(filepath):
+    return bottle.static_file(filepath, root="js")
+
+
+@app.get("/<filepath:re:.*\.json>")
+def js(filepath):
+    return bottle.static_file(filepath, root="")
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
---
+++ readme.txt
@@ -0,0 +1,4 @@
+запуск: app.py (Python 3.5)
+host='localhost', port=8080
+
+работоспособность проверена в firefox, chrome, ie9+.
